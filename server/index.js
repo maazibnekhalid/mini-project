0a47314244b8a976b88cd8335bc760c8fb648e99
Maaz <maazibnekhalid@gmail.com>
2026-04-10 16:27:07 +0500

    Implemented JWT authentication (register/login) with secure password hashing

--- server/index.js
+++ server/index.js
@@ -23,3 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
---
+++ server/models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+/**
+ * User Schema
+ * Stores authentication details
+ */
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
